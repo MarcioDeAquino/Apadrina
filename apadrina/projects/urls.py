e565a2369fc09f66406b407dd7589553d850b936
Rafael Prado <rps4@cin.ufpe.br>
2019-06-16 10:17:07 -0300

    Página inicial estática

--- apadrina/projects/urls.py
+++ apadrina/projects/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import  views
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('', views.home, name='main'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
