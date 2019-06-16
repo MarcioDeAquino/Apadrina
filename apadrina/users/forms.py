0d223c4c5e78164720423b63d33eddb9a5c477a3
Rafael Prado <rps4@cin.ufpe.br>
2019-06-16 13:26:07 -0300

    :P

--- apadrina/users/forms.py
+++ apadrina/users/forms.py
@@ -6,25 +6,20 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class SignUpForm(UserCreationForm):
-    birth_date = forms.DateField(help_text='Required. Format: YYYY-MM-DD')
-    bio = forms.CharField(max_length=500, required=True)
-    gender = forms.ChoiceField( choices=User.GENDERS, required=True)
-    phone = forms.CharField(max_length=15, required=True)
-    description = forms.CharField( max_length=500, required=True)
+    gênero = forms.ChoiceField( choices=User.GENDERS, required=True)
+    celular = forms.CharField(max_length=15, required=True)
     email = forms.EmailField(required=True)
 
     class Meta:
         model = User
-        fields = ('username', 'birth_date', 'password1', 'password2','bio','phone','description')
+        fields = ('username', 'email', 'password1', 'password2','celular', 'gênero')
 
     
     def save(self, commit=True):
         user = super(UserCreationForm, self).save(commit=False)
         user.email = self.cleaned_data["email"]
-        user.bio = self.cleaned_data["bio"]
-        user.gender = self.cleaned_data["gender"]
-        user.phone = self.cleaned_data["phone"]
-        user.description = self.cleaned_data["description"]
+        user.gender = self.cleaned_data["gênero"]
+        user.phone = self.cleaned_data["celular"]
         print(user.gender)
         if commit:
             user.save()
--- apadrina/users/models.py
+++ apadrina/users/models.py
@@ -8,15 +8,12 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     bio = models.TextField(max_length=500, blank=True)
-    location = models.CharField(max_length=30, blank=True)
-    birth_date = models.DateField(null=True, blank=True)
     GENDERS = (
         ('F', 'Feminino'),
         ('M', 'Masculino'),
-        ('NB', 'Nao-binario')
+        ('NB', 'Nao-binário')
     ) 
     gender = models.CharField(max_length=2, choices=GENDERS)       
     phone  = models.CharField(max_length=15)
-    description  = models.TextField(max_length=500)
 
 
