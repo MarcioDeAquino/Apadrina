7822846b45abe7505492d8f1253d4380348f0b08
Rafael Prado <rps4@cin.ufpe.br>
2019-06-16 13:59:40 -0300

    DESESPERO HEIN

--- apadrina/users/urls.py
+++ apadrina/users/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
 from . import views
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('cadastro', views.signup, name='signup'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
